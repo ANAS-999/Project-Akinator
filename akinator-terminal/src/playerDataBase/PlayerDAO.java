1ffbe3f9eeb1ef0062d18b657173b740116f21db
unknown <younesbzize0@gmail.com>
2026-05-17 13:49:36 +0100

    Feat : Adding PlayerDAO

---
+++ akinator-terminal/src/playerDataBase/PlayerDAO.java
@@ -0,0 +1,13 @@
+package playerDataBase;
+
+public class PlayerDAO {
+
+	import java.sql.Connection;
+	import java.sql.DriverManager;
+	import java.sql.PreparedStatement;
+	import java.sql.ResultSet;
+	import java.sql.SQLException;
+	import java.util.ArrayList;
+	import java.util.List;
+	import model.Player; // Import de ta classe Player
+}
