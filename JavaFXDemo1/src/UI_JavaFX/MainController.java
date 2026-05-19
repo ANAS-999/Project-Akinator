1b128596a0a6cf87ce866be1ae1d9731af744e0b
unknown <younesbzize0@gmail.com>
2026-05-19 12:21:38 +0100

    Feat : Added Scenes

---
+++ JavaFXDemo1/src/UI_JavaFX/MainController.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UI_JavaFX;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ *
+ * @author asus
+ */
+public class MainController {
+
+    @FXML
+    private Button clickbtn;
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+    }
+    
+}
